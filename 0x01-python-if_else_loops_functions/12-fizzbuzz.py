4108ba3c7d5147ee63e52de1ab94434085b276fe
Mark Kibara <markkibara@Marks-MacBook-Air.local>
2023-01-06 21:35:03 +0300

    "Modified 12-fizzbuzz.py"

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,6 +8,6 @@ def fizzbuzz():
         elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end='')
         else:
-            print("{:d}, ".format(i), end='')
+            print("{:d}".format(i), end='')
 
         print(" ", end='')
