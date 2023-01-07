65c0a5a18d8ed9aed72066996dbd66928511ba34
Mark Kibara <markkibara@Marks-MacBook-Air.local>
2023-01-07 19:49:24 +0300

    "Modified 13-insert_number.c"

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,7 +9,7 @@
 * Return: the address of the new node, or NULL if it
 * failed.
 */
-listint_t *insert_node(listint_t) ""head, int number)
+listint_t *insert_node(listint_t) **head, int number)
 {
 	listint_t *new;
 	listint_t *h;
