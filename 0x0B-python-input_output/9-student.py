050a1fb5e7991643126e019e002dfb5c4f2a96aa
Mark Kibara <markkibara2014@gmail.com>
2023-01-31 12:34:20 +0300

    Modified 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Module that defines the class Student
-"""
+# 9-student.py
+
+"""Defines a class Student."""
+
 
 class Student:
-	""" Class to create student instances """
-	
-	def __init__(self, first_name, last_name, age):
-		""" Special method to initialize """
-		self.first_name = first_name
-		self.last_name = last_name
-		self.age = age
+    """Represent a student."""
 
-	def to_json(self):
-		"""Method that returns directory description """
-		return self.__dict__copy()
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
