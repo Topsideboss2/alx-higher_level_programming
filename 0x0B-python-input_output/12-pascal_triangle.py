23977d71611cfc0ff1befd92e25a0bf298c57122
Mark Kibara <markkibara2014@gmail.com>
2023-01-31 12:32:01 +0300

    Modified 12-pascal_triange.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+# 12-pascal_triangle.py
+
+"""Defines a Pascal's Triangle function."""
 
 
-class Student:
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
 
-    def __init__(self, first_name, last_name, age):
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-
-        if(type(attrs) == list and all(type(element) == str
-                                       for element in attrs)):
-            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
-        return self.__dict__
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
