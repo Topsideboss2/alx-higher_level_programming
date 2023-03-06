4e714e472930da44660313605fd4aa4071cc30a7
Mark Kibara <markkibara2014@gmail.com>
2023-03-07 00:16:52 +0300

    rm 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_Link.sql
+++
@@ -1,4 +0,0 @@
--- Script that listss all records of a table
--- Query to list all records of the table second_table who have name value
-
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
