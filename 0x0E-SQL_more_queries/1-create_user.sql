4ec6e9f4aebc9b56d47298ec0d712b4179631944
Mark Kibara <markkibara2014@gmail.com>
2023-03-08 10:28:21 +0300

    Modified 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Script that creates a user in MySql server
 -- Query to create the user 'user_0d_1' in MySQL server
-CREATE USER IF NOT EXISIS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost';
