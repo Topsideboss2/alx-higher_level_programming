f78da191f0d973b34e7ef66d6f2e916b7c0e43c6
Topsideboss2 <markkibara2014@gmail.com>
2023-03-16 12:11:56 -0700

    semistandard fixes

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 if (Number.isNaN(process.argv[2])) {
-	console.log("Not a number");
-}
-else {
-	console.log(`My number: ${process.argv[2]}`);
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${process.argv[2]}`);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,14 +3,13 @@
 const number = parseInt(process.argv[2]);
 
 if (isNaN(number)) {
-	console.log("Missing size");
-}
-else {
-	for (let i = 0, side; i < number; i++) {
-		side = '';
-		for (let j = 0; j < number; j++) {
-			side += 'X';
-		}
-		console.log(side);
-	}
+  console.log('Missing size');
+} else {
+  for (let i = 0, side; i < number; i++) {
+    side = '';
+    for (let j = 0; j < number; j++) {
+      side += 'X';
+    }
+    console.log(side);
+  }
 }
