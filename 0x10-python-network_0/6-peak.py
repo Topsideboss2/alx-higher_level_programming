d1e5b2db934a5866f3b976395108e0c4ad5536a6
Mark Kibara <markkibara2014@gmail.com>
2024-01-19 16:29:47 +0300

    Add 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Finds a peak of unsorted integers of a list """
+
+
+def find_peak(list_of_integers):
+    if list_of_integers == []:
+        return None
+
+    length = len(list_of_integers)
+    mid = int(length / 2)
+
+    if mid - 1 < 0 and mid + 1 >= length:
+        return list_of_integers[mid]
+
+    elif mid - 1 < 0:
+        return list_of_integers[mid] if list_of_integers[mid] > list_of_integers[mid + 1] else list_of_integers[mid + 1]
+
+    elif mid + 1 >= length:
+        return list_of_integers[mid] if list_of_integers[mid] > list_of_integers[mid - 1] else list_of_integers[mid - 1]
+
+    if list_of_integers[mid - 1] < list_of_integers[mid] > list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+
+    if list_of_integers[mid + 1] > list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[mid:])
+
+    return find_peak(list_of_integers[:mid])
